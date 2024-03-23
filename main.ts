a59a990979444c38a4e023da4a9b72607a1bd8f4
Muhammad Ayan <158440887+ayanhameedayanhameed@users.noreply.github.com>
2024-03-24 00:45:36 +0500

    adding project 02

---
+++ main.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+
+const answers = await inquirer.prompt([
+  {
+    name: "userGuessedNumber",
+    type: "number",
+    message: "Guess a number between 1 and 10",
+  },
+]);
+
+if (answers.userGuessedNumber === randomNumber) {
+  console.log("Congrats! You got it!");
+} else {
+  console.log("Sorry, try again!");
+}
